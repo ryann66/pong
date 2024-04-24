9bd6acbcf4fa12bb90ef6471229c2ad9393230fb
ryann66 <ryannelson6@hotmail.com>
2024-04-24 13:09:31 -0700

    Implemented keypress handling

--- pong.c
+++ pong.c
@@ -5,15 +5,20 @@
 #endif
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "utils.h"
 
 #define MIN_WINDOW_WIDTH 500
 #define MIN_WINDOW_HEIGHT 400
+#define FRAME_RATE 60
+#define SEC_PER_FRAME 1000 / (FRAME_RATE)
 
 int windowWidth = 800, windowHeight = 600;
 float ballX = 100., ballY = 100., leftPaddleY = 100., rightPaddleY = 100.;
 float ballVelocityX = 10., ballVelocityY = 0.;
+
+bool upButton = false, specialUpButton = false, downButton = false, specialDownButton = false;
 
 // prints the screen
 void display() {
@@ -32,16 +37,37 @@ void reshape(int width, int height) {
 }
 
 void keypress(unsigned char key, int mouseX, int mouseY) {
-    // TODO
-    printf("%c\n", key);
+    if (key == 'w') upButton = true;
+    else if (key == 's') downButton = true;
 }
 
 void specialKeypress(int key, int mouseX, int mouseY) {
-    // TODO
+    if (key == GLUT_KEY_UP) specialUpButton = true;
+    else if (key == GLUT_KEY_DOWN) specialDownButton = true;
+}
+
+void keyrelease(unsigned char key, int mouseX, int mouseY) {
+    if (key == 'w') upButton = false;
+    else if (key == 's') downButton = false;
+}
+
+void specialKeyrelease(int key, int mouseX, int mouseY) {
+    if (key == GLUT_KEY_UP) specialUpButton = false;
+    else if (key == GLUT_KEY_DOWN) specialDownButton = false;
 }
 
 void fixedUpdate(int value) {
-    // TODO
+    if ((downButton || specialDownButton) ^ (upButton || specialUpButton)) {
+        if (downButton || specialDownButton) {
+            // down, TODO
+            printf("down\n");
+        } else if (upButton || specialUpButton) {
+            // up, TODO
+            printf("up\n");
+        }
+    }
+
+    glutTimerFunc(SEC_PER_FRAME, fixedUpdate, 0);
 }
 
 int main(int argc, char* argv) {
@@ -55,7 +81,9 @@ int main(int argc, char* argv) {
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keypress);
     glutSpecialFunc(specialKeypress);
-    glutTimerFunc(500 /*millisecs between ball updates*/, fixedUpdate, 0);
+    glutKeyboardUpFunc(keyrelease);
+    glutSpecialUpFunc(specialKeyrelease);
+    glutTimerFunc(SEC_PER_FRAME, fixedUpdate, 0);
     
     glutMainLoop();
 }
