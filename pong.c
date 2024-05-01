f73a025eb7377512d03d0441769862715065d48b
Ryan Nelson <ryannelson6@hotmail.com>
2024-04-30 23:55:43 -0700

    Changed header include to freeglut

--- pong.c
+++ pong.c
@@ -1,11 +1,11 @@
 #ifdef __APPLE_CC__
 #include <GLUT/gl.h>
 #include <GLUT/glu.h>
-#include <GLUT/glut.h>
+#include <GLUT/freeglut.h>
 #else
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 #endif
 
 #include <stdbool.h>
